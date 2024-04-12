6d644a3e0d155dfcb857ec97a133c90137515ec0
Ekaterina Antonova <missis.katia-antonova@yandex.ru>
2024-04-12 22:20:42 +0300

    Реализованно домашнее задание

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,15 @@
+const dropdownList = document.querySelector(".dropdown__list");
+const dropdown = dropdownList.closest(".dropdown");
+const dropdownItem = Array.from(document.querySelectorAll(".dropdown__item"));
+const dropdownValue = document.querySelector(".dropdown__value");
+
+dropdown.onclick = function() {
+    dropdownList.classList.toggle("dropdown__list_active");
+}
+
+dropdownItem.forEach(item => {
+    item.onclick = function() {
+        dropdownValue.textContent = item.textContent;
+        return false;
+    }
+})
---
+++ event-object/tabs/task.js
@@ -0,0 +1,21 @@
+const tab = Array.from(document.querySelectorAll(".tab"));
+const tabContent = Array.from(document.querySelectorAll(".tab__content"));
+
+const clearActiveClass = (element, className = "tab_active") => {
+    element.find(item => item.classList.remove(`${className}`));
+}
+
+const setActiveClass = (element, index, className = "tab_active") => {
+    element[index].classList.add(`${className}`);
+}
+
+const checkoutTabs = (item, index) => {
+    item.addEventListener("click", () => {
+        clearActiveClass(tab);
+        clearActiveClass(tabContent, "tab__content_active");
+        setActiveClass(tab, index);
+        setActiveClass(tabContent, index, "tab__content_active");
+    })
+}
+
+tab.forEach(checkoutTabs);
